46710c12545ec3c65254d3c24018e9d531870a55
Jac Wang <lu7766lu7766@gmail.com>
2025-10-07 17:00:50 +0800

    變數，邏輯運算子

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+2
+true
+false
+;("hello world")
+null
+undefined
+
+let a = 10
+let b = 20
+let c = a
+a = b
+b = c
+console.log(a, b)
+
+// 邏輯運算子
+a > b // false
+a < b // true
+a == b // false
+a != b // true
+a < b && a != b // true
+a < b || a == b // true
